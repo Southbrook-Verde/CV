e682421c0c7850d390f0d1f455442f535d2fa6cf
Southbrook Verde <Jorgensorheim@live.no>
2021-10-20 22:48:00 +0200

    daily commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
+
+
 smoothScroll = function() {
     var element = document.getElementById( 'div2' );
     element.scrollIntoView({behavior: "smooth"});
+}
+
+function aboutMe() {
+    var screen = document.getElementById("screen");
+    var screenGrid = document.getElementById("screenGrid");
+    var classes = screen.classList;
+    var gridClasses = screenGrid.classList;
+    classes.add("text-left", "pl-2");
+    while (document.getElementById("screen").classList("text-left", "ml-5") === true) {
+        gridClasses.add("grid-col-2", "grid")
+    }
 }
