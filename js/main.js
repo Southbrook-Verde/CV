f3d87e9b8f1027a2d867d7fccd5610f36f900d99
Southbrook Verde <Jorgensorheim@live.no>
2021-10-24 21:26:07 +0200

    usual commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 
-
 smoothScroll = function() {
     var element = document.getElementById( 'div2' );
     element.scrollIntoView({behavior: "smooth"});
-}
-
-function aboutMe(screen) {
-    var screen = document.getElementById("screenContainer3");
-    var classes = screen.classList;
-    classes.toggle("laptop:grid");
 }
